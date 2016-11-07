457a48e8f4cb4b9cde35c409455ad41367fde876
Erling Tokheim <e@tokheim.no>
2016-11-07 23:49:09 +0100

    **** Finalize file. **** -Clean-up, improved comments, etc.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,12 +6,12 @@ var bio = {
 	"contacts": {
 		"mobile": "+47 000 00 000",
 		"email": "e@tokheim.no",
-		"github": "http://github.com/etokheim",
-		"twitter": "etokheim",
+		"github": "etokheim",
+		"twitter": "@ErlingTokheim",
 		"location": "Sandeid"
 	},
 
-	"welcomeMessage": "Welcome dear traveler!",
+	"welcomeMessage": "Welcome, dear traveler, to my awesome resume website!",
 
 	"skills": [
 		"Awesome",
@@ -19,8 +19,9 @@ var bio = {
 		"Web designer"
 	],
 
-	"biopic": "https://s-media-cache-ak0.pinimg.com/236x/5e/e6/95/5ee695e34856d22c2538a22133f3f4e8.jpg",
+	"biopic": "images/biopic.jpg",
 
+	// Displays the object's content in it's appropriate places
 	"display": function() {
 		var formattedRole = HTMLheaderRole.replace("%data%", this.role);
 		$("#header").prepend(formattedRole);
@@ -34,10 +35,10 @@ var bio = {
 		var formattedTwitter = HTMLtwitter.replace("%data%", this.contacts.twitter);
 		var formattedLocation = HTMLlocation.replace("%data%", this.contacts.location);
 
-		// Concatenates the contact information
+		// Concatenates and displays the contact information
 		var concatenatedContactInfo = formattedMobile + formattedEmail + formattedGithub + formattedTwitter + formattedLocation;
-
 		$("#topContacts").append(concatenatedContactInfo);
+		$("#footerContacts").append(concatenatedContactInfo);
 
 		var formattedBioPic = HTMLbioPic.replace("%data%", this.biopic);
 		$("#header").append(formattedBioPic);
@@ -47,32 +48,41 @@ var bio = {
 
 		$("#header").append(HTMLskillsStart);
 
+		// Loops through and displays an array of skills
 		this.skills.forEach(function(skill) {
 			var formattedSkills = HTMLskills.replace("%data%", skill);
 			$("#skills").append(formattedSkills);
 		});
-
-		$("#footerContacts").append(concatenatedContactInfo);
 	}
 }
 
 var education = {
 	"schools": [
 		{
-			"name": "Lundeneset vidaregåande skule",
+			"name": "Lundeneset High School",
 			"city": "Ølensvåg",
-			"degree": "Generell studiekompetanse",
+			"degree": "Higher Education Entrance Qualification",
 			"major": "None",
-			"location": "Lundeneset vidaregåande skule",
-			"dates": "2016"
+			"location": "Lundeneset High School",
+			"dates": "2013 - 2014"
 		},
+
 		{
-			"name": "Lundeneset vidaregåande skule",
+			"name": "Lundeneset High School",
 			"city": "Ølensvåg",
 			"degree": "Media and communication",
-			"major": "None",
+			"major": "Media and communication",
 			"location": "Lundeneset vidaregåande skule",
-			"dates": "2016"
+			"dates": "2011 - 2013"
+		},
+
+		{
+			"name": "Norwegian Armed Forces Media Centre",
+			"city": "Oslo",
+			"degree": "certificate of apprenticeship",
+			"major": "Media Graphics Artist",
+			"location": "Akershus festning",
+			"dates": "2014 - 2016"
 		}
 	],
 
@@ -92,6 +102,7 @@ var education = {
 		}
 	],
 
+	// Displays the object's content in it's appropriate places
 	"display": function() {
 		// Creates a new div with the education-entry class inside #education
 		$("#education").append(HTMLschoolStart);
@@ -133,37 +144,35 @@ var education = {
 	}
 }
 
-education.display();
-
 var work = {
 	"jobs": [
 		{
-			"employer": "Norske landbrukstenester",
-			"title": "Avløysar",
+			"employer": "Norwegian Agriculture Service",
+			"title": "Relief worker (farm)",
 			"dates": "2011-2012",
-			"description": "J sdfj asdf sd jasdfjlas df asd fjljwel afs dlfjqwelj",
+			"description": "Hard work in a challenging landscape at a sheep farm. Never boring and ever challenging. The work included, but was not limited to maintenance, reparations, field work, feeding the animals and most importantly securing food for the animals during the winter.",
 			"location": "Laupsa"
 		},
 
 		{
 			"employer": "Coop Norway",
-			"title": "Co worker",
+			"title": "Shop assistant",
 			"dates": "2010-2010",
-			"description": "J sdfj asdf sd jasdfjlas df asd fjljwel afs dlfjqwelj",
+			"description": "As a shop assistant my job included customer relations, supplying merchandise and making sure everything ran smoothly.",
 			"location": "Øystese"
 		}
 	],
 
+	// Displays the object's content in it's appropriate places
 	"display": function() {
 		for(job in work.jobs) {
 			// create new div for work experiences
 			$("#workExperience").append(HTMLworkStart);
 
-			// Concat employer and title
+			// Concatenate employer and title
 			var formattedEmployer = HTMLworkEmployer.replace("%data%", this.jobs[job].employer);
 			var formattedTitle = HTMLworkTitle.replace("%data%", this.jobs[job].title);
-			var formattedEmployerTitle = formattedEmployer + formattedTitle;
-			$(".work-entry:last").append(formattedEmployerTitle);
+			$(".work-entry:last").append(formattedEmployer + formattedTitle);
 
 			var formattedDates = HTMLworkDates.replace("%data%", this.jobs[job].dates);
 			$(".work-entry:last").append(formattedDates);
@@ -180,69 +189,43 @@ var work = {
 var projects = {
 	"projects": [
 		{
-			"title": "Web design project",
+			"title": "Meme gathering project",
 			"dates": "2016",
-			"description": "This project was very cool!",
-			"images": ["https://s-media-cache-ak0.pinimg.com/236x/5e/e6/95/5ee695e34856d22c2538a22133f3f4e8.jpg"]
+			"description": "I learned a lot from this project. It really took my meme understanding to a whole new level!",
+			"images": ["images/meme1.jpg", "images/meme2.jpg", "images/meme3.jpg"]
 		}
 	],
 
 	"display": function() {
+		for(project in projects.projects) {
+			// create new div for work experiences
+			$("#projects").append(HTMLprojectStart);
 
-	}
-}
+			var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			$(".project-entry:last").append(formattedTitle);
 
+			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+			$(".project-entry:last").append(formattedDates);
 
-work.display();
-bio.display();
+			var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+			$(".project-entry:last").append(formattedDescription);
 
-$(document).click(function(loc) {
-  logClicks(loc.clientX, loc.clientY);
-});
-
-function logClicks(x, y) {
-	console.log(x, y);
-}
-
-
-function locationizer(workObj) {
-	var arrayOfJobs = [];
-	for (job in work.jobs) {
-		arrayOfJobs.push(work.jobs[job].location);
-	}
-
-	return arrayOfJobs;
-}
-
-locationizer(work);
-
-projects.display = function() {
-	for(project in projects.projects) {
-		// create new div for work experiences
-		$("#projects").append(HTMLprojectStart);
-
-		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-		$(".project-entry:last").append(formattedTitle);
-
-		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-		$(".project-entry:last").append(formattedDates);
-
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-		$(".project-entry:last").append(formattedDescription);
-
-		if(projects.projects[project].images.length > 0) {
-			for (image in projects.projects[project].images) {
-				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-				$(".project-entry:last").append(formattedImage);
+			if(projects.projects[project].images.length > 0) {
+				for (image in projects.projects[project].images) {
+					var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+					$(".project-entry:last").append(formattedImage);
+				}
 			}
 		}
 	}
 }
 
+// Runs the display functions to make the content appear.
+work.display();
+bio.display();
 projects.display();
+education.display();
 
-// var location = ["Oslo", "Sidney", "Andorra"];
+// Creates the google map
 $("#mapDiv").append(googleMap);
 initializeMap();
-
-// pinPoster({"locations": ["Oslo", "Sidney", "Andorra"]});
